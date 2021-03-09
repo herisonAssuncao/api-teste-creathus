28fbfc98d8298c6055154f9753a5ef7f9533b3db
Hérison <herison.assuncao@outlook.com>
2021-03-09 19:25:20 -0400

    Adicionando ambiente docker e correções em auth

--- src/main/java/br/com/herison/testecreathus/config/SecurityConfig.java
+++ src/main/java/br/com/herison/testecreathus/config/SecurityConfig.java
@@ -3,12 +3,15 @@ package br.com.herison.testecreathus.config;
 import br.com.herison.testecreathus.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 //@Profile(value = {"prod"})
 @EnableWebSecurity
@@ -29,6 +32,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable().httpBasic();
     }
 }
