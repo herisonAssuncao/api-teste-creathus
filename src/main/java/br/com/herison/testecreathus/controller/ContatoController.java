1766860d215497afbd616a82aef8868efe7467c9
Hérison <herison.assuncao@outlook.com>
2021-03-10 08:33:27 -0400

    Correção cors

--- src/main/java/br/com/herison/testecreathus/controller/ContatoController.java
+++ src/main/java/br/com/herison/testecreathus/controller/ContatoController.java
@@ -12,6 +12,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/contatos")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ContatoController {
 
     @Autowired
--- src/main/java/br/com/herison/testecreathus/controller/UsuarioController.java
+++ src/main/java/br/com/herison/testecreathus/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/usuarios")
+@CrossOrigin(origins = "http://example.com", allowedHeaders = "*")
 public class UsuarioController {
 
     @GetMapping("auth")
